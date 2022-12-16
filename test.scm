9c2a29938a2453f3470b9260cdbf855d9d9c5ff5
Giuseppe Pitton <pitton@protonmail.com>
2022-12-16 10:20:38 +0000

    Enabled two concurrent servers (html, plain text)

--- test.scm
+++ test.scm
@@ -1,3 +1,4 @@
+(use-modules (ice-9 threads))
 (use-modules (sxml simple))
 (use-modules (web server)
              (web request)
@@ -12,73 +13,54 @@
     `(html (head (title ,title))
            (body ,@body)))
 
-(display
-   "const elem = document.getElementById('read'); \
-    const hdr = new Headers(); \
-    hdr.append('Accept', '*/*'); \
-    hdr.append('Accept-Language', 'en-US,en;q=0.5'); \
-    const req = new Request('http://localhost:8000', \
-        { method: 'GET', \
-          action: 'num', \
-          headers: hdr, \
-          mode: 'cors', \
-          referer: 'client', \
-          cache: 'no-cache', \
-          origin: 'http://localhost' \
-        });
-    fetch(new Request('http://localhost:8000', \
-     {method: 'GET', action: 'num'})) \
-        .then((response) => { \
-            const t = response.text(); \
-            console.log(t); \
-            if (!response.ok) { \
-                throw new Error(`HTTP error: ${response.status}`); \
-            } \
-            return t; \
-        }) \
-        .then((str) => elem.textContent = `success: ${str}`) \
-        .catch((err) => elem.textContent = `${err}`);")
-
 (define (serve-page port)
     (let* ((doctype "<!DOCTYPE html>\n")
            (script-str
-   "const elem = document.getElementById('read'); \
-    const hdr = new Headers(); \
-    hdr.append('Accept', '*/*'); \
-    hdr.append('Accept-Language', 'en-US,en;q=0.5'); \
-    const req = new Request('http://localhost:8000', \
-        { method: 'GET', \
-          action: 'num', \
-          headers: hdr, \
-          mode: 'cors', \
-          referer: 'client', \
-          cache: 'no-cache', \
-          origin: 'http://localhost' \
+   "const elem = document.getElementById('read');
+    const hdr = new Headers();
+    const req = new Request('http://localhost:8000',
+        { method: 'GET',
+          action: 'num',
+          headers: hdr,
         });
-    fetch(new Request('http://localhost:8000', \
-     {method: 'GET', action: 'num'})) \
-        .then((response) => { \
-            const t = response.text(); \
-            console.log(t); \
-            if (!response.ok) { \
-                throw new Error(`HTTP error: ${response.status}`); \
-            } \
-            return t; \
-        }) \
-        .then((str) => elem.textContent = `success: ${str}`) \
+    fetch(new Request('http://localhost:8000',
+     {method: 'GET', action: 'num'}))
+        .then((response) => {
+            const t = response.text();
+            console.log(t);
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+            return t;
+        })
+        .then((str) => elem.textContent = `success: ${str}`)
         .catch((err) => elem.textContent = `${err}`);")
            (body `((p "I just read:")
-                   (p (@ (id "read")))
+                   ;; empty string necessary to force the parser
+                   ;; to add a </p> element.
+                   (p (@ (id "read")) "")
                    (script ,script-str))))
     (display doctype port)
     (sxml->xml (fill-template "Fetch API test" body) port)))
-    ;(sxml->xml (fill-template "Fetch API test" '((p "Hello"))) port)))
-        
+
+(define* (respond-num request #:optional body
+            #:key
+            (status 200)
+            (content-type 'text/plain)
+            ;(content-type-params '((charset . "utf-8")))
+            (content-type-params '())
+            ;(access-control-allow-origin "*")
+            ;(extra-headers '((access-control-allow-origin . "*"))))
+            (extra-headers '()))
+    (values (build-response
+             #:code status
+             #:headers `((content-type . (,content-type ,@content-type-params))
+                         ,@extra-headers))
+            (lambda (port) (serve-num port))))
 
 (define* (respond request #:optional body
             #:key
             (status 200)
-            ;; TODO: change to content-type: 'text/plain for sending raw data
             (content-type 'text/html)
             ;(content-type-params '((charset . "utf-8")))
             (content-type-params '())
@@ -89,13 +71,15 @@
              #:code status
              #:headers `((content-type . (,content-type ,@content-type-params))
                          ,@extra-headers))
-            (lambda (port)
-                (cond ((eq? port 8001) (serve-page port))
-                      ((eq? port 8000) (serve-num port))
-                      (else (serve-page port))))))
+            (lambda (port) (serve-page port))))
 
+(define (test-server-num request body)
+    (respond-num body (request-headers request)))
 (define (test-server request body)
     (respond body (request-headers request)))
 
-(run-server test-server 'http '(#:port 8001))
+;; TODO provide a way to quit execution
+(parallel
+    (run-server test-server-num 'http '(#:port 8000))
+    (run-server test-server 'http '(#:port 8001)))
 
