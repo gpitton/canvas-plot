ceb30406390f729e389152d1b5586229f5cdafcd
Giuseppe Pitton <pitton@protonmail.com>
2022-12-16 20:44:19 +0000

    Concurrent server: first working prototype.

--- test.rkt
+++ test.rkt
@@ -5,19 +5,20 @@
 
 
 (define (serve-num req)
-    (response/output
-         (lambda (op) (write (random) op))
-         #:mime-type #"text/plain; charset=utf-8"))
+  (response/output
+   (lambda (op) (write (random) op))
+   #:mime-type #"text/plain; charset=utf-8"
+   #:headers (list (make-header #"Access-Control-Allow-Origin" #"*"))))
 
 (define (fill-template title body)
-    `(html (head (title ,title))
-           (body ,@body)))
+  `(html (head (title ,title))
+         (body ,@body)))
 
 ;; TODO find a better way to generate javascript
 (define (serve-page req)
-    (let* (;(doctype "<!DOCTYPE html>\n")
-           (script-str
-   "const elem = document.getElementById('read');
+  (let* (;(doctype "<!DOCTYPE html>\n")
+         (script-str
+          "const elem = document.getElementById('read');
     const hdr = new Headers();
     const req = new Request('http://localhost:8000',
         { method: 'GET',
@@ -36,25 +37,28 @@
         })
         .then((str) => elem.textContent = `success: ${str}`)
         .catch((err) => elem.textContent = `${err}`);")
-           (body `((p "I just read:")
-                   (p ((id "read")))
-                   (script ,script-str))))
+         (body `((p "I just read:")
+                 (p ((id "read")))
+                 (script ,script-str))))
     (response/output
-         (lambda (op) (parameterize ([current-unescaped-tags (cons 'id html-unescaped-tags)])
-                          (write-xexpr (fill-template "Fetch API test" body) op))))))
+     (lambda (op) (parameterize ([current-unescaped-tags (cons 'id html-unescaped-tags)])
+                    (write-xexpr (fill-template "Fetch API test" body) op))))))
 
+;; main loop
+(define text-server
+  (thread (lambda ()
+            (serve/servlet serve-num
+                           #:port 8000
+                           #:servlet-path "/"
+                           #:command-line? #t))))
 
-;; TODO provide a way to quit execution/run in parallel
-#|
-(serve/servlet serve-num
-               #:port 8000
-               #:servlet-path "/"
-               #:command-line? #t
-               #:stateless? #f)
-|#
-(serve/servlet serve-page
-               #:port 8001
-               #:servlet-path "/"
-               #:command-line? #t
-               #:stateless? #f)
+(define http-server
+  (thread (lambda ()
+            (serve/servlet serve-page
+                           #:port 8001
+                           #:servlet-path "/"
+                           #:command-line? #t))))
 
+(sleep 120)
+(kill-thread text-server)
+(kill-thread http-server)
